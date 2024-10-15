dbeebee469a7d72823a105976b43b7746e60b31d
AlejandroExpoh <alejandro.s280401@cesurformacion.com>
2024-10-15 09:40:39 +0200

    Meteoritos 2

--- Navesita/Assets/Scripts/meteoritos.cs
+++ Navesita/Assets/Scripts/meteoritos.cs
@@ -8,12 +8,14 @@ public class meteoritos : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     Vector2 startPos;
+    public BoxCollider2D gridarea;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
         Launch();
+
     }
 
     // Update is called once per frame
@@ -30,5 +32,23 @@ public class meteoritos : MonoBehaviour
         
     }
 
+    public void randompos()
+    {
+        Bounds limites = gridarea.bounds;
+
+        float x = Random.Range(limites.min.x, limites.max.x);
+        float y = Random.Range(limites.min.y, limites.max.y);
+
+        transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("espacio"))
+        {
+            randompos();
+        }
+
+    }
 
 }
